72b420bb1755c1419b2fa8e28193f08ba80c6c74
ithil0 <fernanda.rubio@sansano.usm.cl>
2022-10-04 15:49:32 -0300

    update vale

--- Back/schemas/vale.js
+++ Back/schemas/vale.js
@@ -59,8 +59,54 @@ const valeResolvers = {
       await temp.save();
       return temp;
     },
-    async updateVale(obj, {id, input}){
-      return await Vale.findByIdAndUpdate(id, input);
+    async updateVale(obj, {id, input}){      
+      let valeGet = await Vale.findById(id);
+      if(valeGet===null){//si se intenta usar un vale que ya fue eliminado
+        return{
+          message:"Vale Inválido" 
+        }
+      } else{//si el vale es válido
+        let estado = valeGet.estado;
+        let saldo = valeGet.saldo;               
+        let servicioGet = await Servicio.findById(servicio);           
+        if(saldo!=0){
+          estado +=1; //se registra un uso
+          input.estado=estado;
+          if(servicioGet.valor===saldo){//se usa todo el saldo
+            saldo=0;  //si se usa todo el vale cuenta como utilizado
+            input.saldo=0;
+            await Funcionario.updateOne(
+              {_id: input.funcionario},
+              {$inc: {valesNoUtilizados: -1, valesUtilizados: 1}}
+            )      
+          }
+          else if(servicioGet.valor<saldo){//queda saldo a favor
+            saldo = saldo - servicio.valor; //se resta el saldo según valor del servicio
+            input.saldo=saldo;
+          }      
+          else{//falta saldo para la compra
+            let saldoTemp=saldo;
+            saldo=0;//si se usa todo el vale cuenta como utilizado
+            input.saldo=0;
+            await Funcionario.updateOne(
+              {_id: input.funcionario},
+              {$inc: {valesNoUtilizados: -1, valesUtilizados: 1}}
+            )
+            return{
+              message:"Se debe pagar la diferencia de " + (servicio.valor-saldoTemp) 
+            }
+          }  
+
+        }      
+        else{//no tiene saldo
+          return{
+            message:"Este vale agotó su saldo disponible" 
+          }
+        } 
+        
+        return await Vale.findByIdAndUpdate(id, input);
+      }
+      
     },
     async deleteVale(obj, {id}){
       await Vale.deleteOne({_id: id});
